aa7fc945262e243373611536542468adea7c3609
loicbourgois <loic@bourgois.pro>
2019-05-04 18:50:29 +0200

    Deploy

--- docs/bootstrap.js
+++ docs/bootstrap.js
@@ -55,6 +55,21 @@
 /******/ 		"../pkg/gravitle_bg.wasm": function() {
 /******/ 			return {
 /******/ 				"./gravitle": {
+/******/ 					"__wbg_log_db0ce4382904de06": function(p0i32,p1i32) {
+/******/ 						return installedModules["../pkg/gravitle.js"].exports["__wbg_log_db0ce4382904de06"](p0i32,p1i32);
+/******/ 					},
+/******/ 					"__wbg_new_59cb74e423758ede": function() {
+/******/ 						return installedModules["../pkg/gravitle.js"].exports["__wbg_new_59cb74e423758ede"]();
+/******/ 					},
+/******/ 					"__wbg_stack_558ba5917b466edd": function(p0i32,p1i32) {
+/******/ 						return installedModules["../pkg/gravitle.js"].exports["__wbg_stack_558ba5917b466edd"](p0i32,p1i32);
+/******/ 					},
+/******/ 					"__wbg_error_4bb6c2a97407129a": function(p0i32,p1i32) {
+/******/ 						return installedModules["../pkg/gravitle.js"].exports["__wbg_error_4bb6c2a97407129a"](p0i32,p1i32);
+/******/ 					},
+/******/ 					"__wbindgen_object_drop_ref": function(p0i32) {
+/******/ 						return installedModules["../pkg/gravitle.js"].exports["__wbindgen_object_drop_ref"](p0i32);
+/******/ 					},
 /******/ 					"__wbindgen_throw": function(p0i32,p1i32) {
 /******/ 						return installedModules["../pkg/gravitle.js"].exports["__wbindgen_throw"](p0i32,p1i32);
 /******/ 					}
@@ -156,7 +171,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/gravitle_bg.wasm":"191849934eda9d1ed6c5"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/gravitle_bg.wasm":"ec602ea73f7b35a87b7b"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
