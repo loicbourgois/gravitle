6dfcf99fa58ae29e3f196425423d60e272c781ad
loicbourgois <loic@bourgois.pro>
2019-05-06 19:09:22 +0200

    Rename segment to link

--- src/intersection.rs
+++ src/intersection.rs
@@ -1,14 +1,14 @@
 use wasm_bindgen::prelude::*;
 
 //
-// Intersection point between a moving particle and a static segment
+// Intersection point between a moving particle and a static link
 //
 #[wasm_bindgen]
 #[derive(Copy, Clone)]
 pub struct Intersection {
     intersection_x: f64,
     intersection_y: f64,
-    segment_id: usize,
+    link_id: usize,
     particle_id: usize
 }
 
@@ -23,13 +23,13 @@ impl Intersection {
     //
     pub fn new (intersection_x: f64,
             intersection_y: f64,
-            segment_id: usize,
+            link_id: usize,
             particle_id: usize
     ) -> Intersection {
         Intersection {
             intersection_x,
             intersection_y,
-            segment_id,
+            link_id,
             particle_id
         }
     }
@@ -43,8 +43,8 @@ impl Intersection {
     pub fn get_intersection_y(& self) -> f64 {
         self.intersection_y
     }
-    pub fn get_segment_id(& self) -> usize {
-        self.segment_id
+    pub fn get_link_id(& self) -> usize {
+        self.link_id
     }
     pub fn get_particle_id(& self) -> usize {
         self.particle_id
