3d18e8463a1b08ffad907d9d7a0e207574f0d48b
Loïc Bourgois <loic@bourgois.pro>
2022-10-27 23:55:07 +0200

    up

---
+++ gravitle_simd/go.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cargo run --release --manifest-path $HOME/github.com/loicbourgois/gravitle/gravitle_simd/Cargo.toml
---
+++ gravitle_simd/src/main.rs
@@ -0,0 +1,64 @@
+#![feature(portable_simd)]
+#![feature(test)]
+extern crate test;
+use test::Bencher;
+use std::simd::f32x16;
+
+
+fn add (a: &mut f32x16, b: & f32x16) {
+    *a = *a + b;
+}
+
+
+fn add2 (a: &mut [f32], b: & [f32] ) {
+    for i in 0..16 {
+        a[i] = a[i] + b [i];
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    // #[test]
+    // fn it_works() {
+    //     assert_eq!(4, add_two(2));
+    // }
+
+    #[bench]
+    fn bench_add(bencher: &mut Bencher) {
+        let mut a = f32x16::from_array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
+        let b = f32x16::from_array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
+        bencher.iter(|| {
+            add(&mut a, &b)
+        });
+    }
+
+    #[bench]
+    fn bench_add2(bencher: &mut Bencher) {
+        let mut a = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        let b = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0];
+        bencher.iter(|| {
+            add2(&mut a, &b)
+        });
+    }
+}
+
+
+fn main() {
+    // let mut a = f32x4::from_array([0.0, 0.0, 0.0, 0.0]);
+    // let b = f32x4::from_array([1.0, 1.0, 1.0, 1.0]);
+    // for _ in 0..10_000 {
+    //     add(&mut a, &b);
+    // }
+    //
+    // let mut a2 = [0.0, 0.0, 0.0, 0.0];
+    // let b2 = [1.0, 1.0, 1.0, 1.0];
+    // for _ in 0..10_000 {
+    //     add2(&mut a2,&b2);
+    // }
+    // println!("{:?}", a);
+    // println!("{:?}", a2);
+}
