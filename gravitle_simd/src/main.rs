cc7cfb96555fd185c585ef0b72ce773d7fe109a9
Loïc Bourgois <loic@bourgois.pro>
2022-10-28 08:52:05 +0200

    up

--- gravitle_simd/src/main.rs
+++ gravitle_simd/src/main.rs
@@ -22,11 +22,6 @@ mod tests {
     use super::*;
     use test::Bencher;
 
-    // #[test]
-    // fn it_works() {
-    //     assert_eq!(4, add_two(2));
-    // }
-
     #[bench]
     fn bench_add(bencher: &mut Bencher) {
         let mut a = f32x16::from_array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
