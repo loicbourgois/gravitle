0d4234a0d90b1ea6cd84677125042af8142b2730
Loïc Bourgois <loic@bourgois.pro>
2022-11-02 09:38:11 +0100

    up

--- front-2/index.js
+++ front-2/index.js
@@ -4,9 +4,6 @@ import {
   fill_text,
   clear,
 } from "./canvas.js"
-
-console.log("plouf")
-
 document.body.innerHTML = `
   <canvas id="canvas"></canvas>
   <div id="right"></div>
@@ -14,26 +11,16 @@ document.body.innerHTML = `
 const right = document.querySelector("#right");
 const canvas = document.querySelector("#canvas");
 const context = canvas.getContext('2d')
-
 resize_square(canvas)
-
-
-
 const socket = new WebSocket('ws://localhost:8080');
-
-// Connection opened
 socket.addEventListener('open', (event) => {
     socket.send('Hello Server!');
 });
-
 socket.binaryType = "arraybuffer";
-
 let image = context.createImageData(canvas.width, canvas.height);
 let data = image.data;
 const dim = canvas.width;
-// console.log(canvas.width)
-
-function drawPixel(x, y, c) {
+const drawPixel = (x, y, c) => {
   	let roundedX = Math.round(x*dim);
   	let roundedY = Math.round(y*dim);
   	let index = 4 * (canvas.width * roundedY + roundedX);
@@ -42,20 +29,16 @@ function drawPixel(x, y, c) {
     data[index + 2] = c.b;
     data[index + 3] = c.a;
 }
-
-
 const colors = [
   	{r: 220, g: 220, b:   0, a: 255},
   	{r: 220, g: 0,   b:   0, a: 255},
+  	{r: 0,   g: 255, b:   0, a: 255},
 ];
-
-// Listen for messages
 let refreshing = false
 console.log(socket)
 socket.addEventListener('message', (event) => {
   if (!refreshing && event.data instanceof ArrayBuffer) {
     refreshing = true
-
     const view = new DataView(event.data);
     let ii = -4;
     const step = view.getInt32(ii+=4)
@@ -82,19 +65,14 @@ socket.addEventListener('message', (event) => {
       const x = view.getFloat32(ii + oi*i)
       const y = view.getFloat32(ii + 4 + oi*i)
       const colliding = view.getInt32(ii + 8 + oi*i)
-      // const thid = view.getInt32(ii + 12 + oi*i)
-      // const fidx = view.getInt32(ii + 16 + oi*i)
       let color = colors[0]
-        if (colliding) {
-          color = colors[1]
-        }
+      if (colliding) {
+        color = colors[1]
+      }
       drawPixel(x,y, color);
     }
     clear(context)
     context.putImageData(image, 0, 0);
     refreshing = false
-  } else {
-    // text frame
-    // console.log(event.data);
-  }
+  } else {  }
 });
