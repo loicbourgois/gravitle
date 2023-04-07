3907d1f1dd458f1212c08e15ebb2f250c8652995
Loïc Bourgois <loic@bourgois.pro>
2023-04-07 20:14:32 +0200

    up

--- front/build.sh
+++ front/build.sh
@@ -2,6 +2,7 @@
 # $HOME/github.com/loicbourgois/gravitle/front/build.sh
 cd $HOME/github.com/loicbourgois/gravitle/gravithrust
 cargo fmt
-wasm-pack -vvv build --target web
-cp -r $HOME/github.com/loicbourgois/gravitle/gravithrust/pkg/ $HOME/github.com/loicbourgois/gravitle/front/gravithrust/
-rm $HOME/github.com/loicbourgois/gravitle/front/gravithrust/package.json
+cargo clippy
+# wasm-pack -vvv build --target web
+# cp -r $HOME/github.com/loicbourgois/gravitle/gravithrust/pkg/ $HOME/github.com/loicbourgois/gravitle/front/gravithrust/
+# rm $HOME/github.com/loicbourgois/gravitle/front/gravithrust/package.json
