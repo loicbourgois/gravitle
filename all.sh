3235863c70cbaba5ea870418a782d9a1ed196a75
Loïc Bourgois <loic@bourgois.pro>
2025-10-11 21:35:32 +0200

    up

--- all.sh
+++ all.sh
@@ -7,5 +7,5 @@ $HOME/github.com/loicbourgois/gravitle/test.sh
 $HOME/github.com/loicbourgois/gravitle/build_mining.sh
 $HOME/github.com/loicbourgois/gravitle/format.sh
 $HOME/github.com/loicbourgois/gravitle/deploy.sh
-# $HOME/github.com/loicbourgois/gravitle/front_deployed.sh
+$HOME/github.com/loicbourgois/gravitle/front_deployed.sh
 # $HOME/github.com/loicbourgois/gravitle/front.sh
